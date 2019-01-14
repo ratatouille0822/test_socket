b09f484ca995dbf087bbdb9c1a18010c9cf09626
Ratatouille <307338947@qq.com>
2019-01-15 01:04:18 +0800

    TCP服务器和客户端的练习代码

--- Test_socket_server.py
+++ Test_socket_server.py
@@ -5,17 +5,24 @@ def main():
     tcp_server_socket = socket(AF_INET, SOCK_STREAM)
     tcp_server_socket.bind(("", 30000))
     tcp_server_socket.listen(128)
-    print("*" * 50)
-    client_socket, client_addr = tcp_server_socket.accept()
-    print("*" * 50)
-    print(client_socket)
-    print(client_addr)
+    while True:
+        print("等待一个新的客户到来： ")
+        client_socket, client_addr = tcp_server_socket.accept()
+        print("来了一个新客户")
+        order = input("请输入： ")
+        if order == "exit":
+            break
+        print(client_socket)
+        print(client_addr)
 
-    receive_data = client_socket.recv(1024)
+        receive_data = client_socket.recv(1024)
 
-    print(receive_data)
+        print(receive_data.decode("utf-8"))
 
-    client_socket.send("--OK--".encode("gbk"))
+        client_socket.send("--OK--".encode("gbk"))
+        client_socket.close()
+
+    tcp_server_socket.close()
 
 
 if __name__ == "__main__":
